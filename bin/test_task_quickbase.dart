00cade6491515b0718110402dd51e8dc1a4adaa4
DemonHashira <v.logodazhki@outlook.com>
2024-05-29 16:00:04 +0300

    Finished main entry point

--- bin/test_task_quickbase.dart
+++ bin/test_task_quickbase.dart
@@ -1,7 +1,24 @@
-import 'package:test_task_quickbase/test_task_quickbase.dart' as test_task_quickbase;
+import 'package:test_task_quickbase/github_freshdesk.dart';
+import 'package:dotenv/dotenv.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${test_task_quickbase.calculate()}!');
+void main(List<String> arguments) async {
+  final dotenv = DotEnv()..load();
+  final githubToken = dotenv['GITHUB_TOKEN']!;
+  final freshdeskToken = dotenv['FRESHDESK_TOKEN']!;
+  final freshdeskDomain = dotenv['FRESHDESK_DOMAIN']!;
+  final username = arguments[0];
+
+  final githubFreshdesk = GitHubFreshDesk(
+    githubToken: githubToken,
+    freshdeskToken: freshdeskToken,
+    freshdeskDomain: freshdeskDomain,
+  );
+
+  try {
+    final user = await githubFreshdesk.getGitHubUser(username);
+    await githubFreshdesk.createOrUpdateFreshdeskContact(user);
+    print('Contact created or updated successfully.');
+  } catch (e) {
+    print('An error occurred: $e');
+  }
 }
-
-
--- lib/github_freshdesk.dart
+++ lib/github_freshdesk.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:dotenv/dotenv.dart' as dotenv;
 import 'database_helper.dart';
 
 class GitHubFreshDesk {
