a709cc1c8551cde120b924bf29942c937748d546
DemonHashira <v.logodazhki@outlook.com>
2024-05-29 14:24:49 +0300

    Added GET PUT and CREATE methods for freshdesk

--- lib/github_freshdesk.dart
+++ lib/github_freshdesk.dart
@@ -5,13 +5,13 @@ import 'database_helper.dart';
 
 class GitHubFreshDesk {
   final String githubToken;
-  final String freshDeskToken;
+  final String freshdeskToken;
   final String freshdeskDomain;
   final DatabaseHelper dbHelper = DatabaseHelper();
 
   GitHubFreshDesk({
     required this.githubToken,
-    required this.freshDeskToken,
+    required this.freshdeskToken,
     required this.freshdeskDomain,
   });
 
@@ -33,6 +33,59 @@ class GitHubFreshDesk {
       return user;
     } else {
       throw Exception('Failed to load GitHub user');
+    }
+  }
+
+  // Create or update the Freshdesk contact
+  Future<void> createOrUpdateFreshdeskContact(Map<String, dynamic> user) async {
+    final email = user['email'];
+    final response = await http.get(
+      Uri.parse(
+          'https://$freshdeskDomain.freshdesk.com/api/v2/contacts?email=$email'),
+      headers: {
+        'Authorization':
+            'Basic ${base64Encode(utf8.encode('$freshdeskToken:x'))}',
+        'Content-Type': 'application/json'
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final contacts = jsonDecode(response.body) as List;
+      if (contacts.isNotEmpty) {
+        final contactId = contacts.first['id'];
+        final updateResponse = await http.put(
+          Uri.parse(
+              'https://$freshdeskDomain.freshdesk.com/api/v2/contacts/$contactId'),
+          headers: {
+            'Authorization':
+                'Basic ${base64Encode(utf8.encode('$freshdeskToken:x'))}',
+            'Content-Type': 'application/json'
+          },
+          body: jsonEncode({'name': user['name'], 'email': user['email']}),
+        );
+
+        if (updateResponse.statusCode != 200) {
+          throw Exception(
+              'Failed to update Freshdesk contact: ${updateResponse.body}');
+        }
+      } else {
+        final createResponse = await http.post(
+          Uri.parse('https://$freshdeskDomain.freshdesk.com/api/v2/contacts'),
+          headers: {
+            'Authorization':
+                'Basic ${base64Encode(utf8.encode('$freshdeskToken:x'))}',
+            'Content-Type': 'application/json'
+          },
+          body: jsonEncode({'name': user['name'], 'email': user['email']}),
+        );
+
+        if (createResponse.statusCode != 201) {
+          throw Exception(
+              'Failed to create Freshdesk contact: ${createResponse.body}');
+        }
+      }
+    } else {
+      throw Exception('Failed to fetch Freshdesk contact: ${response.body}');
     }
   }
 }
